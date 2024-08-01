a1635069426d3a512130f9a1d0483649dac952f1
qequy <llianellpp.pp@gmail.com>
2024-08-02 00:15:00 +0300

    'upd'

--- registration.cpp
+++ registration.cpp
@@ -2,6 +2,11 @@
 #include "ui_registration.h"
 
 #include <QLineEdit>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QCryptographicHash>
 
 Registration::Registration(QWidget *parent)
     : QDialog(parent)
@@ -16,3 +21,22 @@ Registration::~Registration()
 {
     delete ui;
 }
+
+void Registration::on_registrationBtn_clicked()
+{
+    QString login = this->ui->loginLine->text();
+    QString password = this->ui->passLine->text();
+    // QString hashedPassword = QString(QCryptographicHash::hash(password.toUtf8(), QCryptographicHash::Sha256).toHex());
+
+    QSqlQuery query;
+    query.prepare("INSERT INTO users VALUES (:login, :password)");
+    query.bindValue(":login", login);
+    query.bindValue(":password", password);
+
+    if (!query.exec()) {
+        qDebug() << "Failed to register: " << query.lastError();
+    }
+
+    qDebug() << "User registered successfully!";
+}
+
--- registration.h
+++ registration.h
@@ -15,6 +15,9 @@ public:
     explicit Registration(QWidget *parent = nullptr);
     ~Registration();
 
+private slots:
+    void on_registrationBtn_clicked();
+
 private:
     Ui::Registration *ui;
 };
